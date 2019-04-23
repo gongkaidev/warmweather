54ba86762e69df19858db7eb6bc6f39de91d8b91
GK <gk@example.com>
2019-04-23 15:32:11 +0800

    完成遍历

--- app/src/main/java/com/example/warmweather/db/Province.java
+++ app/src/main/java/com/example/warmweather/db/Province.java
@@ -13,8 +13,10 @@ public class Province extends DataSupport {
     public void setId(int id){this.id =id ;}
 
     public String getProvinceName(){return provinceName;}
-    public void getProvinceName(String provinceName){this.provinceName=provinceName;}
+    public void setProvinceName(String provinceName){this.provinceName=provinceName;}
 
     public int getProvinceCode(){return provinceCode;}
     public void setProvinceCode(int provinceCode){this.provinceCode= provinceCode;}
+
+
 }
